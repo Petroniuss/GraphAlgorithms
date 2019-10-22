0f0192ebd37efaf7844552c4f933a6adadd966fd
TemporaryResident <Patrikos112233@interia.pl>
2019-10-22 22:43:21 +0200

    added tester

--- Laboratory/Lab_2/tester.py
+++ Laboratory/Lab_2/tester.py
@@ -4,6 +4,9 @@ import os
 
 ex_1_tests_directory = "Laboratory\\Lab_2\\tests\\flow"
 ex_1_tests = [os.fsdecode(file) for file in os.listdir(ex_1_tests_directory)]
+ex_1_success = 0
+
+print("-" * 10 + "TESTS" + "-" * 10)
 
 for test in ex_1_tests:
     print("Testing...", test)
@@ -16,3 +19,7 @@ for test in ex_1_tests:
     print("solution {0}, actual {1}".format(solution, actual), end=" ")
     if actual == solution:
         print(u"\u2713")
+        ex_1_success = ex_1_success + 1
+print("-" * 50)
+print("Finished testing Ford_Fulkerson {0}/{1} ".format(ex_1_success, len(ex_1_tests)) + u"\u2713")
+print("-" * 50)
