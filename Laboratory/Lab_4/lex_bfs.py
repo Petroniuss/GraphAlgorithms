86c5dd1776933b24221afefc247b2cdec3805cad
TemporaryResident <Patrikos112233@interia.pl>
2019-11-30 19:49:06 +0100

    finished lex-bfs

--- Laboratory/Lab_4/lex_bfs.py
+++ Laboratory/Lab_4/lex_bfs.py
@@ -1,47 +1,44 @@
-from dimacs import *
-
+from dimacs import loadDirectedWeightedGraph
 
 class Node:
   def __init__(self, idx):
     self.idx = idx
-    self.out = set()              # zbiór sąsiadów
+    self.out = set()                    # set of neighbours
 
-  def connect_to(self, v):
+  def connect_to(self, v):              # add edge between node (u) and node (v)
     self.out.add(v)
 
-def lex_bfs(G, start):
-    V = len(G) 
+def lex_bfs(G, start = 1):
+    V = len(G)
 
-    visited = [False] * V
-
-    full_set = set([i for i in range(1, V+1) if i != start])
-    start_set = set({start})
+    full_set = set([G[i] for i in range(1, V) if i != start])
+    start_set = set({G[start]})
 
     sets = [full_set, start_set]
     result = []
 
-    while len(sets) > 0:
+    while len(sets) > 0: 
         u = sets[-1].pop()
-        result.append(u)
-        visited[u] = True
+        result.append(u.idx)
 
-        new_sets_list = []
+        new_sets = []
+        for X in sets:
+            Y = set()
+            K = set()
+            
+            for v in X:
+                if v in u.out:
+                    Y.add(v)
+                else:
+                    K.add(v)
 
-        for set in sets[:]:
-            neigh_set = set()
-            not_neigh_set = set()
+            new_sets.append(K)
+            new_sets.append(Y)    
+        
+        while len(new_sets) > 0 and not new_sets[-1]:
+            new_sets.pop()
 
-            for node in set:
-                if not visited[node.idx]:
-                    if node in u.out():
-                        neigh_set.add(node)
-                    else:
-                        not_neigh_set.add(node)
-
-            new_sets_list.append(not_neigh_set)
-            new_sets_list.append(neigh_set)
-
-        sets = new_sets_list
+        sets = new_sets
 
     return result
 
@@ -53,3 +50,5 @@ def run(name):
     for (u, v, _) in L:
         G[u].connect_to(v)
         G[v].connect_to(u)
+
+    return G, lex_bfs(G)
---
+++ Laboratory/Lab_4/tester_1.py
@@ -0,0 +1,32 @@
+from lex_bfs import run
+from dimacs import checkLexBFS
+import os
+
+"""
+    To exectute in top directory run: py Laboratory/Lab_4/tester_1.py
+"""
+
+ex_1_tests_directory = "Laboratory\\Lab_4\\tests\\coloring"
+ex_1_tests = [os.fsdecode(file) for file in os.listdir(ex_1_tests_directory)]
+ex_1_success = 0
+
+print("-" * 20 + "TESTS" + "-" * 20)
+
+for test in ex_1_tests:
+    print("Testing...", test)
+
+    test_path = ex_1_tests_directory + "\\" + test
+
+    G, vs = run(test_path)
+
+    print(vs)
+
+    if checkLexBFS(G, vs):
+        print(u"\u2713", end= "")
+        ex_1_success = ex_1_success + 1
+    
+    print("\n")
+
+print("-" * 50)
+print("Finished testing lex-bfs {0}/{1} ".format(ex_1_success, len(ex_1_tests)) + u"\u2713")
+print("-" * 50)
